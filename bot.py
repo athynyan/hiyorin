07e020b9c7aa066ebf1ce22970fd126b1e9d3e5e
Thanh Than Trung <thantrungthanh>
2021-01-01 13:44:47 +0100

    updated to run through env

--- bot.py
+++ bot.py
@@ -4,4 +4,4 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='?')
 bot.load_extension(f'cogs.main')
 
-bot.run('NzcyMjEyNDc2NjI0MjQwNjkw.X53Y6Q.zjIHSxoc5PYIxOJmNjljGYWJ2dU')
+bot.run(os.environ('TOKEN'))
