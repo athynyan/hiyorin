b4169c83fb1a0155dc9fc53143296196996b022f
Thanh Than Trung <thantrungthanh@gmail.com>
2021-08-22 16:04:17 +0200

    refractoring tl module

--- modules/helpers/tl.py
+++ modules/helpers/tl.py
@@ -1,3 +1,4 @@
+import discord
 from utils.postgres import get_character_dicts
 import re
 
@@ -63,3 +64,15 @@ def format_tl_input(input_string):
             temporary_list.append(element)
 
     return '\n '.join(general_info), '\n '.join(party_setup), '\n '.join(timeline), ''
+
+
+def make_tl_embed(input_string):
+    general, party, timeline, additional_info = format_tl_input(input_string)
+
+    embed = discord.Embed(color=0x657ed4)
+    embed.add_field(name='General Info', value=general, inline=False)
+    embed.add_field(name='Party Setup', value=party, inline=False)
+    embed.add_field(name='Timeline', value=timeline, inline=False)
+    if additional_info:
+        embed.add_field(name='Additional Info', value=additional_info, inline=False)
+    return embed
